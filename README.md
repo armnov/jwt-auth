d0c5301375d543d3f1eeab429ec3598797a37f86
Armanov <armanov@mba.local>
2019-12-30 22:38:24 +0800

    add jwt with claims

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # jwt-auth
 A simple example of using JWT auth in Golang. This example is using jwt-go library.
+
+Example output:
+
+App started...
+Generating tokens
+eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1Nzc3MTY3MTksImlzcyI6Im15IGFwcCJ9.yuduMdXXvxBewa7xZ4ROSkhQl0zq_ItOB0Ncwq7C-fQ
+1.577716719e+09 my app
+====== with claims ======
+Token with claims generated:  eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1Nzc3MTY3MTksImlzcyI6Ik1ZX0FQUCJ9.avFO5lPbHE7Wd11a8CPnSf3Lvr3mSjh6UB3G6RP8v20
+Claims valid:   1577716719 MY_APP
--- main.go
+++ main.go
@@ -10,6 +10,11 @@ var JWT_SECRET = []byte("secret_sekali")
 var TOKEN_DURATION = 60 * time.Second
 var token string
 
+type MyClaims struct {
+	username string `json:"username"`
+	jwt.StandardClaims
+}
+
 func main() {
 	fmt.Println("App started...")
 	fmt.Println("Generating tokens")
@@ -18,6 +23,11 @@ func main() {
 	token = generateToken()
 	fmt.Println(token)
 	validateToken(token)
+
+	fmt.Println("====== with claims ======")
+
+	tokenWithClaims := generateTokenWithClaims()
+	validateTokenWithClaims(tokenWithClaims)
 
 }
 
@@ -58,4 +68,44 @@ func validateToken(t string) {
 	} else {
 		fmt.Println("Error when parsing token: ", err.Error())
 	}
+}
+
+func generateTokenWithClaims() string {
+
+	//create claims
+	claims := MyClaims{
+		"admin",
+		jwt.StandardClaims{
+			ExpiresAt: time.Now().UTC().Add(TOKEN_DURATION).Unix(),
+			Issuer:    "MY_APP",
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	ss, err := token.SignedString(JWT_SECRET)
+
+	if err != nil {
+		fmt.Println("Error generating token with claims: ", err.Error())
+	}
+
+	fmt.Println("Token with claims generated: ", ss)
+
+	return ss
+}
+
+func validateTokenWithClaims(t string) {
+	token, err := jwt.ParseWithClaims(t, &MyClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(JWT_SECRET), nil
+	})
+
+	if err != nil {
+		fmt.Println("Error parsing token: ", err.Error())
+	}
+
+	if claims, ok := token.Claims.(*MyClaims); ok && token.Valid {
+		fmt.Println("Claims valid: ", claims.username, claims.StandardClaims.ExpiresAt, claims.Issuer)
+	} else {
+		fmt.Println("Somethign is wrong with the claims")
+	}
+
 }
